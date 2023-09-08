32e24b7ffced5cc329770c77e89111b5c4cd14bd
Shrinidhi Nayak <123535419+NidhiNayak2002@users.noreply.github.com>
2023-09-08 21:33:57 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -9,13 +9,15 @@ public class Main {
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
-    for(int presentCap : presentCapacities){
-      int soh = calculateSoh(presentCap);
-      String classification = classifyBattery(soh);
-      if(classification.equals("Healthy")){
+    
+    for(int capacity : presentCapacities){
+      double sohPercentage = (capacity * 100.0) / 120.0; // Assuming rated capacity is 120 Ah
+      
+      //Conditions to check SoH of batteries
+      if(sohPercentage > 80){
         counts.healthy++;
       }
-      else if(classification.equals("Exchange")){
+      else if(sohPercentage >= 65){
         counts.exchange++;
       }
       else{
@@ -24,47 +26,46 @@ public class Main {
     }
     return counts;
   }
-  
-  public static int calculateSoh(int presentCap){//calculate the soh
-    return (presentCap/120)*100;
-  }
-  
-  public static String classifyBattery(int soh){//checks and returns the battery status
-    if(soh>80){
-      return "Healthy";
-    }
-    else if(soh>=65 ){
-      return "Exchange";
-    }
-    else{
-      return "Failed";
-    }
-    
-  }
-  
-   public static void main(String[] args) {
+
+  public static void main(String[] args)  {
     System.out.println("Counting batteries by SoH...\n");
-    
-    int[] presentCapacities = {115, 118, 80, 95, 91, 77};//test case 1
+
+    // Given test case
+    int[] presentCapacities = {115, 118, 80, 95, 91, 77};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
 
-    int[] presentCapacities1 = {};//test case 2
-    CountsBySoH counts2 = countBatteriesByHealth(presentCapacities1);
+    
+    // test with empty array
+    int[] capacities2 = {};
+    CountsBySoH counts2 = countBatteriesByHealth(capacities2);
     assert(counts2.healthy == 0);
     assert(counts2.exchange == 0);
     assert(counts2.failed == 0);
-    
-    int[] presentCapacities2 = {64,60,62,65,63};//test case 3
-    CountsBySoH counts3 = countBatteriesByHealth(presentCapacities2);
-    assert(counts3.healthy == 0);
+
+   // test with all the batteries healthy
+    int[] capacities3 = {120,119,120,118,120};
+    CountsBySoH counts3 = countBatteriesByHealth(capacities3);
+    assert(counts3.healthy == 5);
     assert(counts3.exchange == 0);
-    assert(counts3.failed == 5);
+    assert(counts3.failed == 0);
+
+// test with all the batteries need exchange
+    int[] capacities4 = {84,80,82,85,83};
+    CountsBySoH counts4 = countBatteriesByHealth(capacities4);
+    assert(counts4.healthy == 0);
+    assert(counts4.exchange == 5);
+    assert(counts4.failed == 0);
+
+// test with all the batteries failed
+    int[] capacities5 = {30,40,45,55,58};
+    CountsBySoH counts5 = countBatteriesByHealth(capacities5);
+    assert(counts5.healthy == 0);
+    assert(counts5.exchange == 0);
+    assert(counts5.failed == 5);
     
     System.out.println("Done counting :)\n");
-   
   }
-   
 }
